9952d769ac40a31e2010f00618ac22f02c1dcd4a
Dina <andrianambintsoadina@Gmail.com>
2025-09-30 09:35:01 +0300

    adding sliderInput

--- WeatherViz.r
+++ WeatherViz.r
@@ -28,33 +28,55 @@ tabPanel("Visualization",
 sidebarLayout(
 mainPanel(width = 9,
     fluidRow(
-     column(6,plotOutput("plot1"),textOutput("summarie"),verbatimTextOutput("infos")),
-     column(6,plotOutput("plot2",hover = "hover_click"),"Coordinates",verbatimTextOutput("hover2"))),
+     column(6,plotOutput("plot1"),uiOutput("slider1"),textOutput("summarie"),verbatimTextOutput("infos")),
+     column(6,plotOutput("plot2",hover = "hover_click"),uiOutput("slider2"),"Coordinates",verbatimTextOutput("hover2"))),
     )
 ,
 sidebarPanel(width = 3,
     fileInput("data","Upload data",accept = c(".csv",".xlsx")),
     selectInput("cols","Select parameters",choices = NULL),
     actionButton("btn","Refresh columns",class = "btn-success"),
-    selectInput("colors","Choose colors",choices = c("#2997ab","#2cdf85","black","#db5e73ff","grey","rgba(150, 13, 143, 0.8)")),
+    selectInput("colors","Choose colors",choices = c("#2997ab","#2cdf85","black","#db5e73ff","grey")),
     )))))
     
 
 server = function(input,output,session){
+
     df2 = reactive({read_excel(input$data$datapath)})
     df_year = reactive({pere_year(df2())})
     df_month = reactive({monthly(df2())})
-    month_summary = reactive({summary(df_month()[[input$cols]])})
-
-
+       
+    output$slider1 = renderUI({
+    req(input$data)
+    req(input$cols)
+    req(input$colors)
+    sliderInput("s1","Filter value",max = as.integer(max(df_month()[[input$cols]])),min = as.integer(min(df_month()[[input$cols]])),value = as.integer(max(df_month()[[input$cols]])))
+    })
+    output$slider2 = renderUI({
+    req(input$data)
+    req(input$cols)
+    req(input$colors)
+    sliderInput("s2","Filter value",max = as.integer(max(df_year()[[input$cols]])),min = as.integer(min(df_year()[[input$cols]])),value = as.integer(max(df_year()[[input$cols]])))
+    })
+    df_year2 = reactive({
+    req(input$s2)    
+    df_year()%>%
+    filter(.data[[input$cols]] <= input$s2)})
+    df_month2 = reactive({
+    req(input$s1)
+    df_month()%>%
+    filter(.data[[input$cols]] <= input$s1)
+    })
+    month_summary = reactive({summary(df_month2()[[input$cols]])})
 observeEvent(input$btn,{updateSelectInput(inputId = "cols",choices = colnames(df2())[2:12])})
 output$plot1 = renderPlot({
     req(input$data)
     req(input$cols)
     req(input$colors)
-    ggplot(data = df_month())+
-    geom_point(mapping = aes(x = df_month()[["year"]],y = df_month()[[input$cols]]),color = input$colors)+
-    facet_wrap(~month(df_month()[["month"]],label = TRUE,abbr = FALSE))+
+
+    ggplot(data = df_month2())+
+    geom_point(mapping = aes(x = df_month2()[["year"]],y = df_month2()[[input$cols]]),color = input$colors)+
+    facet_wrap(~month(df_month2()[["month"]],label = TRUE,abbr = FALSE))+
     labs(x = "date",y = input$cols)+
     theme_light()
 })
@@ -62,10 +84,11 @@ output$plot2 = renderPlot({
     req(input$data)
     req(input$cols)
     req(input$colors)
-    ggplot(data = df_year())+
-    geom_point(mapping = aes(x = year,y = df_year()[[input$cols]]),color = input$colors)+
-    geom_line(mapping = aes(x = year,y = df_year()[[input$cols]]))+
-    geom_smooth((mapping = aes(x = year,y = df_year()[[input$cols]])),color = input$colors)+
+
+    ggplot(data = df_year2())+
+    geom_point(mapping = aes(x = year,y = df_year2()[[input$cols]]),color = input$colors)+
+    geom_line(mapping = aes(x = year,y = df_year2()[[input$cols]]))+
+    geom_smooth((mapping = aes(x = year,y = df_year2()[[input$cols]])),color = input$colors)+
     theme_light()+
     labs(x = "year",y = input$cols)
 })
